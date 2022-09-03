679bae01e89c2e137be618a2a1340798576d3315
tochiedev <tochie.dev@gmail.com>
2022-09-03 10:41:29 +0100

    update dropwish

--- components/Rsvp Forms/PersonalInfo.js
+++ components/Rsvp Forms/PersonalInfo.js
@@ -5,6 +5,7 @@ import { useFormData } from "/components/providers/FormProvider";
 import * as yup from "yup";
 import { Form } from "@unform/web";
 import { useAppStates } from "/components/providers/AppContext";
+import dayjs from "dayjs";
 
 const schema = yup.object().shape(
   {
@@ -65,6 +66,10 @@ export default function PersonalInfo({ event, formStep, noRsvp, step }) {
       console.log(err);
     }
   }
+  const isToday = (someDate) => {
+    if (!someDate) return false;
+    return dayjs().isSame(someDate, "day");
+  };
 
   return (
     <>
@@ -76,6 +81,10 @@ export default function PersonalInfo({ event, formStep, noRsvp, step }) {
           autoComplete="off"
         >
           <div className="flex flex-col space-y-8 md:space-y-6 my-auto">
+            <p className="text-center mb-6 text-default">
+              {event.title}
+              <br></br> would love to know who's dropping this wish!
+            </p>
             <div className="relative">
               <TextInput
                 label="Full Name"
@@ -111,32 +120,49 @@ export default function PersonalInfo({ event, formStep, noRsvp, step }) {
           </div>
 
           {/* <div className="h-3"></div> */}
-          <div className=" justify-between items-centerx mt-4">
-            <p className="text-sm text-center text-pink-600 font-bold pb-4">
-              Ready to party with us?
-            </p>
+          {!isToday(event.startDate) && (
+            <div className=" justify-between items-centerx mt-4">
+              <p className="text-sm text-center text-pink-600 font-bold pb-4">
+                Ready to party with us?
+              </p>
 
-            <div className="flex flex-col-reverse md:flex-row md:space-x-4 justify-between ">
-              <button
-                type="submit"
-                className=" my-2 transition duration-150 ease-in-out focus:outline-none rounded text-pink-600 border border-pink-600 px-6 py-3 text-sm w-full"
-                onClick={() => {
-                  attending = false;
-                }}
-              >
-                No, and I'll regret it
-              </button>
-              <button
-                className=" my-2 transition duration-150 ease-in-out focus:outline-none rounded bg-pink-600 text-primary-light border border-pink-600 px-6 py-3 text-sm w-full"
-                onClick={() => {
-                  attending = true;
-                }}
-                type="submit"
-              >
-                Yes, let's get you married
-              </button>
+              <div className="flex flex-col-reverse md:flex-row md:space-x-4 justify-between ">
+                <button
+                  type="submit"
+                  className=" my-2 transition duration-150 ease-in-out focus:outline-none rounded text-pink-600 border border-pink-600 px-6 py-3 text-sm w-full"
+                  onClick={() => {
+                    attending = false;
+                  }}
+                >
+                  No, and I'll regret it
+                </button>
+                <button
+                  className=" my-2 transition duration-150 ease-in-out focus:outline-none rounded bg-pink-600 text-primary-light border border-pink-600 px-6 py-3 text-sm w-full"
+                  onClick={() => {
+                    attending = true;
+                  }}
+                  type="submit"
+                >
+                  Yes, let's get you married
+                </button>
+              </div>
             </div>
-          </div>
+          )}
+          {isToday(event.startDate) && (
+            <div className=" justify-between items-centerx mt-1">
+              <div className="flex flex-col-reverse md:flex-row md:space-x-4 justify-between ">
+                <button
+                  className=" my-2 transition duration-150 ease-in-out focus:outline-none rounded bg-pink-600 text-primary-light border border-pink-600 px-6 py-3 text-sm w-full"
+                  onClick={() => {
+                    attending = true;
+                  }}
+                  type="submit"
+                >
+                  Congratulations!
+                </button>
+              </div>
+            </div>
+          )}
         </Form>
       </div>
     </>
