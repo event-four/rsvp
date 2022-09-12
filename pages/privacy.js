5b303e5af37c93d700549ea385defd20e34d4466
tochiedev <tochie.dev@gmail.com>
2022-09-13 00:27:35 +0100

    add privacy policy

---
+++ pages/privacy.js
@@ -0,0 +1,289 @@
+import Image from "next/image";
+
+const Privacy = () => {
+  return (
+    <div className="flex flex-col container p-10">
+      <h1 className="text-center text-xl font-bold">
+        Privacy Policy: EventFour Apps
+      </h1>
+
+      <h2 className="text-center">
+        <span className="text-sm">Updated on 13th September, 2022</span>
+      </h2>
+
+      <div className="text-center flex flex-row space-x-6 w-full justify-center my-6">
+        <div>
+          <Image src={"/logo.png"} alt="E4 Logo" width={100} height={100} />
+        </div>
+        <div>
+          <Image src={"/e4vendor.png"} alt="E4 Logo" width={100} height={100} />
+        </div>
+      </div>
+
+      <p className="text-justify mb-6">
+        This page is used to inform visitors regarding our policies with the
+        collection, use, and disclosure of Personal Information if anyone
+        decided to use our Service.
+      </p>
+
+      <p className="text-justify mb-6">
+        <span>
+          <>
+            If you choose to use our Service, then you agree to the collection
+            and use of information in relation to this policy. The Personal
+            Information that we collect is used for providing and improving the
+            Service. We will not use or share your information with anyone
+            except as described in this Privacy Policy.
+          </>
+        </span>
+      </p>
+
+      <p className="text-justify mb-6">
+        <span>
+          <>
+            The terms used in this Privacy Policy have the same meanings as in
+            our Terms and Conditions, which is accessible at EventFour app
+            unless otherwise defined in this Privacy Policy.
+          </>
+        </span>
+      </p>
+
+      <p className="text-justify mb-6">
+        <>
+          <strong>Information Collection and Use</strong>
+        </>
+      </p>
+
+      <p className="text-justify mb-6">
+        <span>
+          <>
+            For a better experience, while using our Service, we may require you
+            to provide us with certain personally identifiable information,
+            including but not limited to Android advertising ID.
+          </>
+        </span>
+      </p>
+
+      <p className="text-justify mb-6">
+        <span className="text-lg">
+          <strong>
+            <>Contacts, Photos and File</>
+          </strong>
+        </span>
+      </p>
+
+      <p className="text-justify mb-6">
+        <>
+          <span>
+            Access to some features may require you grant the app access to your
+            contacts (ex. for you to add contacts to your guest list),
+            &nbsp;photos (ex. for a vendor to upload photos of their works), and
+            files (to share a document with other users).&nbsp;
+          </span>
+        </>
+      </p>
+
+      <p className="text-justify mb-6">
+        <>
+          <span>
+            Your information is not shared with any third-party. They are
+            securely kept and you can delete them whenever you wish to.
+          </span>
+        </>
+      </p>
+
+      <p className="text-justify mb-6">
+        <>
+          <strong>Log Data</strong>
+        </>
+      </p>
+
+      <p className="text-justify mb-6">
+        <span>
+          <>
+            We want to inform you that whenever you use our Service, in a case
+            of an error in the app we collect data and information (through
+            third party products) on your phone. This Log Data may include
+            information such as your device Internet Protocol (&ldquo;IP&rdquo;)
+            address, device name, operating system version, the configuration of
+            the app when utilizing our Service, the time and date of your use of
+            the Service, and other statistics.
+          </>
+        </span>
+      </p>
+
+      <p className="text-justify mb-6">
+        <>
+          <strong>Cookies</strong>
+        </>
+      </p>
+
+      <p className="text-justify mb-6">
+        <span>
+          <>
+            Cookies are files with a small amount of data that are commonly used
+            as anonymous unique identifiers. These are sent to your browser from
+            the websites that you visit and are stored on your device&rsquo;s
+            internal memory.
+          </>
+        </span>
+      </p>
+
+      <p className="text-justify mb-6">
+        <span>
+          <>
+            This Service does not use these &ldquo;cookies&rdquo; explicitly.
+            However, the app may use third party code and libraries that use
+            &ldquo;cookies&rdquo; to collect information and improve their
+            services. You have the option to either accept or refuse these
+            cookies and know when a cookie is being sent to your device. If you
+            choose to refuse our cookies, you may not be able to use some
+            portions of this Service.
+          </>
+        </span>
+      </p>
+
+      <p className="text-justify mb-6">
+        <>
+          <strong>Service Providers</strong>
+        </>
+      </p>
+
+      <p className="text-justify mb-6">
+        <span>
+          <>
+            We may employ third-party companies and individuals due to the
+            following reasons:
+          </>
+        </span>
+      </p>
+
+      <ul>
+        <li className="text-justify mb-6">
+          <span>
+            <>To facilitate our Service;</>
+          </span>
+        </li>
+        <li className="text-justify mb-6">
+          <span>
+            <>To provide the Service on our behalf;</>
+          </span>
+        </li>
+        <li className="text-justify mb-6">
+          <span>
+            <>To perform Service-related services; or</>
+          </span>
+        </li>
+        <li className="text-justify mb-6">
+          <span>
+            <>To assist us in analyzing how our Service is used.</>
+          </span>
+        </li>
+      </ul>
+
+      <p className="text-justify mb-6">
+        <span>
+          <>
+            We want to inform users of this Service that these third parties
+            have access to your Personal Information. The reason is to perform
+            the tasks assigned to them on our behalf. However, they are
+            obligated not to disclose or use the information for any other
+            purpose.
+          </>
+        </span>
+      </p>
+
+      <p className="text-justify mb-6">
+        <>
+          <strong>Security</strong>
+        </>
+      </p>
+
+      <p className="text-justify mb-6">
+        <span>
+          <>
+            We value your trust in providing us your Personal Information, thus
+            we are striving to use commercially acceptable means of protecting
+            it. But remember that no method of transmission over the internet,
+            or method of electronic storage is 100% secure and reliable, and we
+            cannot guarantee its absolute security.
+          </>
+        </span>
+      </p>
+
+      <p className="text-justify mb-6">
+        <>
+          <strong>Links to Other Sites</strong>
+        </>
+      </p>
+
+      <p className="text-justify mb-6">
+        <span>
+          <>
+            This Service may contain links to other sites. If you click on a
+            third-party link, you will be directed to that site. Note that these
+            external sites are not operated by us. Therefore, we strongly advise
+            you to review the Privacy Policy of these websites. We have no
+            control over and assume no responsibility for the content, privacy
+            policies, or practices of any third-party sites or services.
+          </>
+        </span>
+      </p>
+
+      <p className="text-justify mb-6">
+        <>
+          <strong>Children&rsquo;s Privacy</strong>
+        </>
+      </p>
+
+      <p className="text-justify mb-6">
+        <span>
+          <>
+            These Services do not address anyone under the age of 16. We do not
+            knowingly collect personally identifiable information from children
+            under 16. In the case we discover that a child under 16&nbsp;has
+            provided us with personal information, we immediately delete this
+            from our servers. If you are a parent or guardian and you are aware
+            that your child has provided us with personal information, please
+            contact us so that we will be able to do necessary actions.
+          </>
+        </span>
+      </p>
+
+      <p className="text-justify mb-6">
+        <>
+          <strong>Changes to This Privacy Policy</strong>
+        </>
+      </p>
+
+      <p className="text-justify mb-6">
+        <span>
+          <>
+            We may update our Privacy Policy from time to time. Thus, you are
+            advised to review this page periodically for any changes. We will
+            notify you of any changes by posting the new Privacy Policy on this
+            page. These changes are effective immediately after they are posted
+            on this page.
+          </>
+        </span>
+      </p>
+
+      <p className="text-justify mb-6">
+        <>
+          <strong>Contact Us</strong>
+        </>
+      </p>
+
+      <p className="text-justify mb-6">
+        <span>
+          <>
+            If you have any questions or suggestions about our Privacy Policy,
+            do not hesitate to contact us at eventfourapps@gmail.com.
+          </>
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Privacy;
