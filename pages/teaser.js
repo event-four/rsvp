d2a6416adf14f22edd84f5ad7936235846803946
tochiedev <tochie.dev@gmail.com>
2022-10-14 13:48:53 +0100

    update pdf viewer

--- pages/teaser.js
+++ pages/teaser.js
@@ -1,24 +1,61 @@
 import Image from "next/image";
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import ReactPDF from "@react-pdf/renderer";
 
-const Teaser = () => {
-  return (
-    <div className="flex flex-col container p-10">
-      <h2 className="text-center">
-        <span className="text-xs">Updated on 14th October, 2022</span>
-      </h2>
+// // Create styles
+// const styles = StyleSheet.create({
+//   page: {
+//     flexDirection: "row",
+//     backgroundColor: "#E4E4E4",
+//   },
+//   section: {
+//     margin: 10,
+//     padding: 10,
+//     flexGrow: 1,
+//   },
+// });
 
-      <div className="text-center flex flex-row space-x-6 w-full justify-center my-6">
-        <div>
-          <Image
-            src={"/e4teaser.jpg"}
-            alt="E4 Teaser"
-            width={1379}
-            height={1800}
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
+import dynamic from "next/dynamic";
 
-export default Teaser;
+const FileViewer = dynamic(() => import("react-file-viewer"), {
+  ssr: false,
+});
+
+export default function Index() {
+  return <FileViewer fileType="pdf" filePath="/e4teaser.pdf" />;
+}
+
+// const Teaser = () => {
+//   return (
+//     <Document>
+//       <Page size="A4" style={styles.page}>
+//         <View style={styles.section}>
+//           <Text>Section #1</Text>
+//         </View>
+//         <View style={styles.section}>
+//           <Text>Section #2</Text>
+//         </View>
+//       </Page>
+//     </Document>
+//     // <div className="flex flex-col container p-10">
+//     //   <h2 className="text-center">
+//     //     <span className="text-xs">Updated on 14th October, 2022</span>
+//     //   </h2>
+
+//     //   <div className="text-center flex flex-row space-x-6 w-full justify-center my-6">
+//     //     <div>
+//     //       <Image
+//     //         src={"/e4teaser.jpg"}
+//     //         alt="E4 Teaser"
+//     //         width={1379}
+//     //         height={1800}
+//     //       />
+//     //     </div>
+//     //   </div>
+//     // </div>
+//   );
+// };
+
+// ReactPDF.render(<Teaser />, `${__dirname}/e4teaser.pdf`);
+
+// export default Teaser;
