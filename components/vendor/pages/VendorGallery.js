69b859a4e0bf902a1eecbe81a1f30f624bd750de
tochiedev <tochie.dev@gmail.com>
2022-11-18 18:29:08 +0100

    added gallery

--- components/vendor/pages/VendorGallery.js
+++ components/vendor/pages/VendorGallery.js
@@ -11,13 +11,14 @@ import Fab from "@mui/material/Fab";
 import { Form } from "@unform/web";
 import { LoadingButton } from "@mui/lab";
 import { useSnackbar } from "../../SnackBar";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 import * as yup from "yup";
 import { userService, dataService } from "/services";
 import {
-  useUpdateVendorProfile,
-  useFetchVendorProfile,
-  useUpdateVendorProfilePhoto,
+  usePostGalleryPhoto,
+  useFetchVendorGallery,
 } from "@/services/vendor-service";
 import FormCard from "@/components/FormCard";
 import FormProvider from "@/components/providers/HostStartFormProvider";
@@ -43,100 +44,46 @@ const schema = yup.object().shape(
 const DshVendorGallery = ({ pageTitle }) => {
   const user = userService.getUser();
   const vendor = user.vendor_profile;
-  const states = dataService.getStates();
-  const countries = dataService.getCountries();
   const snackbar = useSnackbar();
   const [showSpinner, setShowSpinner] = useState(false);
-  const [coverMedia, setCoverMedia] = useState();
+  const fileChooserRef = useRef();
+
   const formRef = useRef();
-  const [state, setStateNg] = useState();
-  const [country, setCountry] = useState({ name: "Nigeria", code: "NG" });
-  const [canWorkOutside, setCanWorkOutside] = useState();
   const [vendorProfile, setVendorProfile] = useState();
-  const logoUploaderRef = createRef();
-  const [logo, setLogo] = useState();
-  const [logoBlob, setLogoBlob] = useState();
+  const [gallery, setGallery] = useState([]);
 
   const [isLoading, setIsLoading] = useState(false);
-  const { data, loading, error } = useFetchVendorProfile(vendor.id);
+  const { data, loading, error } = useFetchVendorGallery(vendor.id);
 
   useEffect(() => {
     if (data) {
-      setVendorProfile(data);
-      setCanWorkOutside(data.canWorkOutside);
-      //get state
-      const state = data.state
-        ? states.find((state) => state.name === data.state)
-        : null;
-      setStateNg(state);
-      //get logo.
-      if (data.avatar) {
-        setLogo(data.avatar);
-      }
+      setGallery(data);
     }
   }, [data]);
 
-  const handleStateChange = (e) => {
-    setStateNg(e);
-  };
-  const handleCountryChange = (e) => {
-    setCountry(e);
-  };
-  const onCanWorkOutsideChange = (checked, e) => {
-    setCanWorkOutside(checked);
-  };
-
-  async function handleSubmit(data) {
-    const { businessName, whyUnique } = data;
-
-    try {
-      setIsLoading(true);
-
-      if (businessName === "" || !state || whyUnique === "") {
-        snackbar.error("The fields marked * are required.");
-        return;
-      }
-      // Validation passed - do something with data
-      const payload = {
-        id: vendor.id,
-        businessName,
-        whyUnique,
-        state: state.name,
-        country: country.name,
-        canWorkOutside,
-      };
-
-      await useUpdateVendorProfile(payload);
-      snackbar.show("Business profile updated successfully.");
-      setIsLoading(false);
-    } catch (err) {
-      const errors = {};
-      // Validation failed - do show error
-      console.log(err);
-      snackbar.error(err.message, "Oops! an error occurred.");
-    }
-
-    setIsLoading(false);
-  }
-
-  const startUpload = () => {
-    logoUploaderRef.current.value = null;
-    logoUploaderRef.current.click();
-  };
-
-  const onReady = async ({ blobImg, base64 }) => {
-    // console.log(res);
-    if (!blobImg) return;
+  const onReady = async (e) => {
+    const { files } = e.target;
+    if (!files) return;
     const formData = new FormData();
+    const data = {
+      // title: "abc",
+      type: "photo",
+      vendor: vendor.id,
+    };
+    Array.from(files).forEach((file) => {
+      formData.append(`files.photo`, file, "bz_gallery_" + vendor.id);
+    });
 
-    formData.append("files", blobImg, "bz_logo_" + vendorProfile.id);
-    formData.append("refId", vendorProfile.id);
-    formData.append("ref", "api::vendor-profile.vendor-profile");
-    formData.append("field", "avatar");
-    formData.append("id", vendorProfile.id);
-
-    await useUpdateVendorProfilePhoto(formData, vendorProfile.id);
-    setLogoBlob(base64);
+    formData.append("data", JSON.stringify(data));
+    setShowSpinner(true);
+    try {
+      const res = await usePostGalleryPhoto(formData, vendor.id);
+      setGallery(res.data);
+    } catch (error) {
+      snackbar.error(error.message);
+      setShowSpinner(false);
+    }
+    setShowSpinner(false);
   };
 
   const onDeleteLogo = async () => {
@@ -157,7 +104,7 @@ const DshVendorGallery = ({ pageTitle }) => {
     setShowSpinner(false);
   };
 
-  const spinner = () => {
+  const Spinner = () => {
     return (
       <div className="flex mx-auto justify-center items-center py-16 left-0 right-0 top-0 bottom-0 transition ease-in-out duration-150 cursor-not-allowed flex-col h-full bg-white bg-opacity-80">
         <svg
@@ -183,141 +130,81 @@ const DshVendorGallery = ({ pageTitle }) => {
     );
   };
 
-  if (!vendorProfile) {
+  if (!gallery) {
     return <div>Loading...</div>;
   }
 
   return (
     <Section title={pageTitle}>
-      <FormProvider>
-        <FormCard classes="pt-0">
-          <Form
-            ref={formRef}
-            onSubmit={handleSubmit}
-            className={`flex flex-col justify-between md:mx-auto`}
-            autoComplete="off"
-          >
-            <section className="overflow-hidden text-gray-700 ">
-              <div className="container py-2 mx-auto">
-                <div className="flex flex-wrap -m-1 md:-m-2">
-                  <div className="flex flex-wrap w-1/3">
-                    <div className="w-full p-1 md:p-2">
+      <input
+        ref={fileChooserRef}
+        className="hidden"
+        type="file"
+        multiple
+        onChange={onReady}
+      />
+      <div
+        ref={formRef}
+        className={`flex flex-col justify-between md:mx-auto`}
+        autoComplete="off"
+      >
+        <section className="overflow-hidden text-gray-700 ">
+          <p>Photos</p>
+          <small className="text-xs text-gray-400">
+            Show off your business! Add photos of your past work.
+          </small>
+          <div className="container py-2 mx-auto">
+            <div className="grid grid-cols-3 gap-2 gap-y-2">
+              {gallery &&
+                gallery.map((g, index) => (
+                  <div key={index} className="flex flex-wrap  ">
+                    <div className="w-full h-40 relative">
                       <img
                         alt="gallery"
-                        className="block object-cover object-center w-full h-full rounded-lg"
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                        className="block object-cover object-center w-full h-full rounded-md"
+                        src={g.photo.thumbnail}
                       />
+                      <div className="absolute top-2 right-2 p-1 bg-white bg-opacity-60 rounded-full">
+                        <IconButton
+                          onClick={() => deletePhoto()}
+                          aria-label="delete"
+                          size="small"
+                        >
+                          <DeleteIcon fontSize="inherit" />
+                        </IconButton>
+                      </div>
                     </div>
                   </div>
-                  <div className="flex flex-wrap w-1/3">
-                    <div className="w-full p-1 md:p-2">
-                      <img
-                        alt="gallery"
-                        className="block object-cover object-center w-full h-full rounded-lg"
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
-                      />
+                ))}
+              <div
+                className="flex flex-wrap border border-dashed hover:border-default hover:text-default hover:cursor-pointer rounded-md"
+                onClick={() => {
+                  if (showSpinner) return;
+                  fileChooserRef.current.click();
+                }}
+              >
+                <div className="w-full h-40 ">
+                  {showSpinner ? (
+                    <Spinner />
+                  ) : (
+                    <div className="flex flex-col  items-center justify-center h-full w-full text-gray-400 hover:text-default ">
+                      <AddCircleOutlineIcon sx={{ fontSize: 40 }} />
+                      <span className="text-sm">Add Photo</span>
+                      <span className="text-xs">Max. 4MB</span>
                     </div>
-                  </div>
-                  <div className="flex flex-wrap w-1/3">
-                    <div className="w-full p-1 md:p-2">
-                      <img
-                        alt="gallery"
-                        className="block object-cover object-center w-full h-full rounded-lg"
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"
-                      />
-                    </div>
-                  </div>
-                  <div className="flex flex-wrap w-1/3">
-                    <div className="w-full p-1 md:p-2">
-                      <img
-                        alt="gallery"
-                        className="block object-cover object-center w-full h-full rounded-lg"
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"
-                      />
-                    </div>
-                  </div>
-                  <div className="flex flex-wrap w-1/3">
-                    <div className="w-full p-1 md:p-2">
-                      <img
-                        alt="gallery"
-                        className="block object-cover object-center w-full h-full rounded-lg"
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(76).webp"
-                      />
-                    </div>
-                  </div>
-                  <div className="flex flex-wrap w-1/3">
-                    <div className="w-full p-1 md:p-2">
-                      <img
-                        alt="gallery"
-                        className="block object-cover object-center w-full h-full rounded-lg"
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
-                      />
-                    </div>
-                  </div>
+                  )}
                 </div>
               </div>
-            </section>
-            <div className="flex flex-col space-y-4 md:space-y-6">
-              <div
-                className={`relative block w-full  text-center  items-center overflow-clip`}
-              >
-                <Upload
-                  id={logoUploaderRef}
-                  setShowSpinner={setShowSpinner}
-                  saveAs={"event.slug"}
-                  typeVideo={true}
-                  lockAspectRatio={1 / 1}
-                  onReady={onReady}
-                />
-                {showSpinner && { spinner }}
-
-                {/* <div className="flex flex-col justify-center items-center">
-                  <button onClick={startUpload} type="button" className="">
-                    {!showSpinner &&
-                      (!logo ? (
-                        <div className="flex rounded-full h-24 w-24 bg-gray-300 items-center justify-center text-white text-4xl">
-                          {vendorProfile.businessName &&
-                            vendorProfile.businessName.charAt(0).toUpperCase()}
-                        </div>
-                      ) : (
-                        <div className="flex flex-col h-24 w-24 items-center justify-center">
-                          <img
-                            className={` h-24 w-24 rounded-full`}
-                            src={`${logoBlob ?? logo.thumbnail}`}
-                          ></img>
-                        </div>
-                      ))}
-                  </button>
-                  <div className="text-xs text-gray-500 mt-4">
-                    <p>Your Business Logo</p>
-                  </div>
-                  <div>
-                    <div className="flex flex-row space-x-4 mt-4">
-                      {logo ? (
-                        <Button
-                          onClick={onDeleteLogo}
-                          variant="outlined"
-                          size="small"
-                        >
-                          Delete
-                        </Button>
-                      ) : (
-                        <Button
-                          onClick={startUpload}
-                          variant="contained"
-                          size="small"
-                        >
-                          Upload
-                        </Button>
-                      )}
-                    </div>
-                  </div>
-                </div> */}
-              </div>
             </div>
-          </Form>
-        </FormCard>
-      </FormProvider>
+          </div>
+        </section>
+        <section className="overflow-hidden text-gray-700 mt-6">
+          <p>Videos</p>
+          <small className="text-xs text-gray-400">
+            Add YouTube videos of your past work.
+          </small>
+        </section>
+      </div>
     </Section>
   );
 };
